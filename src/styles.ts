f9912a361a588250090f2970ebd4ff251f8cddff
Adam Mawlawi <adam@mradam.us>
2022-05-24 22:12:53 -0500

    create isHidden components

--- src/styles.ts
+++ src/styles.ts
@@ -11,12 +11,20 @@ export const AppContainer = styled.div`
   width: 100%;
 `
 
+interface DragPreviewContainerProps {
+  isHidden?: boolean
+}
+
+export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
+  opacity: ${(props) => (props.isHidden ? 0.3 : 1)};
+`
+
 export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
 `
 
-export const CardContainer = styled.div`
+export const CardContainer = styled(DragPreviewContainer)`
   background-color: #fff;
   cursor: pointer;
   margin-bottom: 0.5rem;
@@ -26,7 +34,7 @@ export const CardContainer = styled.div`
   box-shadow: #091e4240 0px 1px 0px 0px;
 `
 
-export const ColumnContainer = styled.div`
+export const ColumnContainer = styled(DragPreviewContainer)`
   background-color: #ebecf0;
   width: 300px;
   min-height: 40px;
