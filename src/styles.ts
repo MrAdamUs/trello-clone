65600ea8cfd9ec158f7038d723bf977f43c2da88
Adam Mawlawi <adam@mradam.us>
2022-05-25 23:26:03 -0500

    Implement the custom dragging preview

--- src/styles.ts
+++ src/styles.ts
@@ -13,10 +13,12 @@ export const AppContainer = styled.div`
 
 interface DragPreviewContainerProps {
   isHidden?: boolean
+  isPreview?: boolean
 }
 
 export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
-  opacity: ${(props) => (props.isHidden ? 0.3 : 1)};
+  transform: ${(props) => (props.isPreview ? "rotate(5deg)" : undefined)};
+  opacity: ${(props) => (props.isHidden ? 0 : 1)};
 `
 
 export const ColumnTitle = styled.div`
@@ -99,3 +101,17 @@ export const CustomDragLayerContainer = styled.div`
   left: 0;
   z-index: 100;
 `
+type DragPreviewWrapperProps = {
+  position: {
+    x: number
+    y: number
+  }
+}
+
+export const DragPreviewWrapper = styled.div.attrs<DragPreviewWrapperProps>(
+  ({ position: { x, y } }) => ({
+    style: {
+      transform: `translate(${x}px, ${y}px)`,
+    },
+  })
+)<DragPreviewWrapperProps>``
